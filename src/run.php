d9652ea10cdad8dfe281dbd004cb44477feaa8cb
TheDigitalProblemSolver <859830+thedigitalproblemsolver@users.noreply.github.com>
2024-07-20 19:37:44 +0200

    Update run.php
    Added use

--- src/run.php
+++ src/run.php
@@ -8,6 +8,7 @@ use Exception;
 use Throwable;
 use VitesseCms\Cli\Utils\CliUtil;
 use VitesseCms\Core\Utils\DirectoryUtil;
+use Phalcon\Http\Request;
 
 require_once __DIR__ . '/../../../autoload.php';
 require_once __DIR__ . '/../../configuration/src/Utils/DomainConfigUtil.php';
